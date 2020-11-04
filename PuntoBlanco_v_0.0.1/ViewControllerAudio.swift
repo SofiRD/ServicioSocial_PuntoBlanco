497fba27d4324576bdd58716cc3c968c2371ab19
Sofia Recinos <A01657055@itesm.mx>
2020-11-04 13:32:18 -0600

    update 1

---
+++ PuntoBlanco_v_0.0.1/ViewControllerAudio.swift
@@ -0,0 +1,29 @@
+//
+//  ViewControllerAudio.swift
+//  PuntoBlanco_v_0.0.1
+//
+//  Created by Sofia Dorst on 04.11.20.
+//
+
+import UIKit
+
+class ViewControllerAudio: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
